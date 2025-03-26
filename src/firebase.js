145e1855e95face7673146d87c8015d23551d8ab
Anastasia Taucci <112522011+AnastasiaTaucci@users.noreply.github.com>
2025-03-26 14:53:25 -0700

    remove second firebase key

--- src/firebase.js
+++ src/firebase.js
@@ -4,13 +4,6 @@ import { getAuth } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
 
 const firebaseConfig = {
-  // apiKey: "AIzaSyDZA8AEo6LOscZWG2of_m7uhqrXKWirT8Q",
-  // authDomain: "movieapp-5d204.firebaseapp.com",
-  // projectId: "movieapp-5d204",
-  // storageBucket: "movieapp-5d204.firebasestorage.app",
-  // messagingSenderId: "759539462570",
-  // appId: "1:759539462570:web:304a201adb62122b093bd3"
-
   apiKey: "AIzaSyDr1GK8oUbiem3-E-nTyFa0Vip2TGGmp5Y",
   authDomain: "movie-app-f9082.firebaseapp.com",
   projectId: "movie-app-f9082",
